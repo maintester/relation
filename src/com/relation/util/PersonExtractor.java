879792cff1f87121b9265a6c6e1280787441eb9f
holger <Holger.Reinecke@gmx.net>
2016-08-15 09:16:15 +0200

    PersonExtractor

---
+++ src/com/relation/util/PersonExtractor.java
@@ -0,0 +1,35 @@
+package com.relation.util;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+import com.relation.models.Person;
+
+public class PersonExtractor {
+
+	public ArrayList<Person> getPersons(Set names, String textline) {
+		ArrayList<Person> persons = new ArrayList<Person>();
+		String[] parts = textline.split(" ");
+		int index = 0;
+		for (String part : parts) {
+			part = part.trim();
+			if (names.contains(part)) {
+				String first = part;
+				String family = "";
+				if (parts.length > index) {
+					family = parts[index + 1];
+				}
+				if (family.length() < 4) {
+					if (index > 1) {
+						family = parts[index - 1];
+					}
+				}
+				System.out.println("I-PER:" + first + ":" + family + ":");
+				persons.add(new Person(first,"",family) );
+			}
+			index++;
+		}
+		return persons;
+	}
+
+}
