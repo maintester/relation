a5cc06313c0a84ec8be39718561bf9b14d0854c8
holger <Holger.Reinecke@gmx.net>
2016-08-15 09:05:31 +0200

    Fileutil fehlte

---
+++ src/com/relation/util/FileUtils.java
@@ -0,0 +1,22 @@
+package com.relation.util;
+
+import java.util.Date;
+
+public class FileUtils {
+
+	public static String getTStamp (){
+		return ""+new Date().getTime();
+	}
+	public static String getItemDelim (){
+		return "|";
+	}
+	public static String getLineDelim (){
+		return "\r\n";
+	}
+	public static String getPathTextFiles(){
+		return "textfiles\\";
+	}
+	public static String getPathNameFiles(){
+		return "namefiles\\";
+	}	
+}
